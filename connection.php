0ceea845fe8b863352b8dcdc663412fd0d84d680
Talitha Aurora Nadenggan Siregar <121077954+nadenggansrgr@users.noreply.github.com>
2023-08-18 22:27:10 +0700

    Update connection.php
    Menambah fungsi search

--- connection.php
+++ connection.php
@@ -15,3 +15,9 @@ function hapus($id){
 	mysqli_query($koneksi,"DELETE FROM keranjang WHERE id = $id");
 	return mysqli_affected_rows($koneksi);
 }
+
+function cari($keyword)
+{
+	$query = "SELECT*FROM produk WHERE nama LIKE '%$keyword%' OR kategori LIKE '%$keyword%' OR lokasi LIKE '%$keyword%'"; //kalau LIKE dan % satu kata (bahkan 1 huruf) aja gpp, nama produk gaharus komplit. OR agar bisa cari berdasaarkan lebih dari 1 field
+	return query($query);
+}
